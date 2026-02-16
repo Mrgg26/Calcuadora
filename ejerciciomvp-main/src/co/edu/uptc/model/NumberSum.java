6570daade8f573918aabb5b9effacf3c478d4166
Mrgg26 <gabogambaleguizamon@gmail.com>
2026-02-15 23:08:16 -0500

    MVP bien aplicado

--- ejerciciomvp-main/src/co/edu/uptc/model/NumberSum.java
+++ ejerciciomvp-main/src/co/edu/uptc/model/NumberSum.java
@@ -1,19 +1,25 @@
 package co.edu.uptc.model;
 
 import co.edu.uptc.interfaces.ModelInterface;
-import co.edu.uptc.interfaces.PresenterInterface;
 
-public class NumberSum implements ModelInterface {
+public class NumberByteSum implements ModelInterface {
 
     @Override
-    public int exec(int number1, int number2) throws Exception {
+    public int exec(String number1, String number2) throws Exception {
 
-        int result = number1 + number2;
-        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
-            throw new Exception("Error del Modelo: El resultado excede la capacidad de un int.");
+        if (number1 == null || number2 == null ||
+                number1.isEmpty() || number2.isEmpty()) {
+            throw new Exception("Los campos no pueden estar vacíos.");
         }
 
-        return (int) result;
+        try {
+            byte n1 = Byte.parseByte(number1);
+            byte n2 = Byte.parseByte(number2);
+
+            return n1 + n2;
+
+        } catch (NumberFormatException e) {
+            throw new Exception("Debe ingresar números válidos tipo byte (-128 a 127).");
+        }
     }
 }
-
